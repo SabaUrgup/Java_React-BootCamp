f13c8e41c8d8527c4fa5c6314115c7f098404e27
Saba ÜRGÜP <76062971+SabaUrgup@users.noreply.github.com>
2021-04-30 13:39:33 +0300

    Delete Course codes/Third Lesson : Encapsulation and Inheritance/Inheritance & Polymorphism/inheritance2 directory

--- Course codes/Third Lesson : Encapsulation and Inheritance/Inheritance & Polymorphism/inheritance2/src/inheritance2/EmailLogger.java
+++
@@ -1,8 +0,0 @@
-package inheritance2;
-
-public class EmailLogger extends Logger {
-	@Override
-	public void log() {
-		System.out.println("Email yollandý");
-	}
-}
--- Course codes/Third Lesson : Encapsulation and Inheritance/Inheritance & Polymorphism/inheritance2/src/inheritance2/FileLogger.java
+++
@@ -1,8 +0,0 @@
-package inheritance2;
-
-public class FileLogger extends Logger  {
-	@Override
-	public void log() {
-		System.out.println("Dosya loglandý");
-	}
-}
