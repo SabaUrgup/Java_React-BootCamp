26190d0d6c996ec55c31b886a2518f8c08681fe8
Saba ÜRGÜP <76062971+SabaUrgup@users.noreply.github.com>
2021-04-30 13:38:20 +0300

    Delete Course codes/Third Lesson : Encapsulation and Inheritance/Inheritance & Polymorphism/inheritance directory

--- Course codes/Third Lesson : Encapsulation and Inheritance/Inheritance & Polymorphism/inheritance/src/inheritance/IndividualCustomer.java
+++
@@ -1,7 +0,0 @@
-package inheritance;
-
-public class IndividualCustomer extends Customer //IndividualCustomer sen bir Customer'sýn.
-	{	String firstName;
-		String lastName;
-		String nationalIdenity;
-	}
--- Course codes/Third Lesson : Encapsulation and Inheritance/Inheritance & Polymorphism/inheritance/src/inheritance/SendikaCustomer.java
+++
@@ -1,5 +0,0 @@
-package inheritance;
-
-public class SendikaCustomer extends Customer //SendikaCustomer sen bir Customer'sın.
-	{String sendikaBiseyi;
-	}
