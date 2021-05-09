334bf032c2adb1b0259490323b78e8e55a058336
Saba ÜRGÜP <76062971+SabaUrgup@users.noreply.github.com>
2021-05-09 13:28:37 +0300

    Northwind Database Script for Postgre Sql

---
+++ Course codes/Sixth Lesson : Northwind/src/main/java/kodlamaio/northwind/business/abstracts/ProductService.java
@@ -0,0 +1,12 @@
+package kodlamaio.northwind.business.abstracts;
+
+import java.util.List;
+
+import kodlamaio.northwind.entities.concretes.Product;
+
+public interface ProductService {
+
+	List<Product> getAll();
+	
+	
+}
